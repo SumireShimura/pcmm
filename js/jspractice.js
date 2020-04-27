bd9fbe5591cf60ee5d3b9035fa377b46448032b7
sumireshimura <shimurasumire@gmail.com>
2020-04-27 14:47:58 +0900

    humburger

--- js/jspractice.js
+++ js/jspractice.js
@@ -1,35 +1,35 @@
-//document.write("Hello World!");
-//alert("Hello World!");
+document.write("Hello World!");
+alert("Hello World!");
 
-//console.log("Hello Wold!" + "田中さん");
-//console.log(10 + 10)
-//console.log("10" + "10")
+console.log("Hello Wold!" + "田中さん");
+console.log(10 + 10)
+console.log("10" + "10")
 
-// onsole.log(1 + 1);
-// console.log(1 - 1);
-// console.log(2 * 2);
-// console.log(10 / 3);
-// console.log(10 % 3);
+onsole.log(1 + 1);
+console.log(1 - 1);
+console.log(2 * 2);
+console.log(10 / 3);
+console.log(10 % 3);
 
-// let x = 1;
-// let y = 1;
-// x++; //x = x + 1
-// console.log(x);
-// y--; //y = y - 1
-// console.log(y);
+let x = 1;
+let y = 1;
+x++; //x = x + 1
+console.log(x);
+y--; //y = y - 1
+console.log(y);
 
-// let x = 7;
-// let y = x++; //xがyに代入されてからインクリメント
-// let x2 = 7;
-// let y2 = ++x2; //x2のインクリメント後にy2に代入
+let x = 7;
+let y = x++; //xがyに代入されてからインクリメント
+let x2 = 7;
+let y2 = ++x2; //x2のインクリメント後にy2に代入
 
-// console.log(x);
-// console.log(y);
-// console.log(x2);
-// console.log(y2);
+console.log(x);
+console.log(y);
+console.log(x2);
+console.log(y2);
 
-// console.log(x++);
-// console.log(++x);
+console.log(x++);
+console.log(++x);
 
 //変数とは　値を代入するための箱
 //定数とは　値を別名で定義したもの
